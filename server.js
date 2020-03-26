604c26b49c4e7a6c31c53e62c1b162fe628017c2
Palash Taneja <tanejapalash@gmail.com>
2020-03-26 13:13:18 -0500

    added admin panel

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+const NUM_BUTTONS = 2;
 var ctr = [0, 0, 0];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/admin', function(req, res){
+  res.sendFile(__dirname + '/admin.html');
 });
 
 io.on('connection', function(socket){
@@ -15,6 +21,13 @@ io.on('connection', function(socket){
     socket.on('button click', function(type) {
         ctr[type] += 1;
         io.emit('button set', {value: ctr[type], type: type});
+    });
+
+    socket.on('clear', function(_) {
+        for(var i = 1; i <= NUM_BUTTONS; ++i) {
+          ctr[i] = 0;
+          io.emit('button set', {value: ctr[i], type: i});
+        }
     });
 });
 
